b9eb96416b5a020defaac75f29f6d742663e3d43
paalamugan <paalamugan@gmail.com>
2022-11-07 19:55:50 +0530

    feat: added progressbar on page change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import '../styles/global.scss';
 
 import type { AppProps } from 'next/app';
+import Router from 'next/router';
 import type { Session } from 'next-auth';
+import NProgress from 'nprogress';
 
 import { AuthLayout } from '@/layouts/AuthLayout';
 import { MainLayout } from '@/layouts/MainLayout';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 interface MyAppProps<TAppProps> extends AppProps<TAppProps> {
   Component: AppProps['Component'] & { requireAuth?: boolean };
