58998b068ced28344bf1621493cea5098f1199d2
paalamugan <paalamugan@gmail.com>
2022-11-07 16:55:07 +0530

    test: added google font mocked

---
+++ __mocks__/@next/font/google.ts
@@ -0,0 +1,8 @@
+export const Roboto = jest.fn().mockReturnValue({
+  style: {
+    fontFamily: "'__Roboto_mocked', '__Roboto_Fallback_mocked'",
+    fontWeight: 400,
+    fontStyle: 'normal',
+  },
+  className: '__className_mocked',
+});
